9c0e31a1e27106d441492ab01c05523fd274ddd0
Adithyan <adithyanm.22cse@kongu.edu>
2024-12-02 13:26:26 +0530

    coffee type

--- src/Components/Services/Service.jsx
+++ src/Components/Services/Service.jsx
@@ -28,8 +28,8 @@ const Service = () => {
 
               <div className="p-2 text-center">
                 <h1 className="text-xl font-bold">Espresso</h1>
-                <p className="hover:text-white duration-300 text-sm line-clamp-2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <p className="hover:text-white duration-300 text-xs text-center line-clamp-2 pt-2">
+                Espresso: bold, rich, and smooth—a tiny cup packed with big energy and flavor!
                 </p>
               </div>
             </div>
@@ -49,8 +49,8 @@ const Service = () => {
 
               <div className="p-2 text-center">
                 <h1 className="text-xl font-bold">Americano</h1>
-                <p className="hover:text-white duration-300 text-sm line-clamp-2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <p className="hover:text-white duration-300 text-xs line-clamp-2 pt-2">
+                Americano: A smooth, bold espresso with a splash of hot water—perfect for those who love rich flavor!
                 </p>
               </div>
             </div>
@@ -69,8 +69,8 @@ const Service = () => {
               </div>
               <div className="p-2 text-center">
                 <h1 className="text-xl font-bold">Cappuccino</h1>
-                <p className="hover:text-white duration-300 text-sm line-clamp-2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <p className="hover:text-white duration-300 text-xs pt-2 line-clamp-2">
+                Cappuccino: A velvety blend of rich espresso, steamed milk, and frothy foam—coffee!
                 </p>
               </div>
             </div>
@@ -89,8 +89,8 @@ const Service = () => {
               </div>
               <div className="p-2 text-center">
                 <h1 className="text-xl font-bold">Latte</h1>
-                <p className="hover:text-white duration-300 text-sm line-clamp-2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <p className="hover:text-white duration-300 text-xs pt-2">
+                Latte: A creamy, comforting mix of espresso and steamed milk, topped with a light foam
                 </p>
               </div>
             </div>
@@ -108,8 +108,8 @@ const Service = () => {
               </div>
               <div className="p-2 text-center">
                 <h1 className="text-xl font-bold">Mocha</h1>
-                <p className="hover:text-white duration-300 text-sm line-clamp-2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <p className="hover:text-white duration-300 text-xs pt-2 line-clamp-2">
+                Mocha: A delightful fusion of rich espresso, steamed milk, and decadent chocolate, topped with whipped cream
                 </p>
               </div>
             </div>
@@ -127,8 +127,8 @@ const Service = () => {
               </div>
               <div className="p-2 text-center">
                 <h1 className="text-xl font-bold">Irish</h1>
-                <p className="hover:text-white duration-300 text-sm line-clamp-2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                <p className="hover:text-white duration-300 text-xs pt-2 line-clamp-2">
+                Irish Coffee: A warm, spirited blend of bold coffee, smooth whiskey, and a touch of cream
                 </p>
               </div>
             </div>
